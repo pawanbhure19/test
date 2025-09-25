4b9fd5c3184c2f01d0ecd0b46fe4609f50360fcf
pawanbhure19 <96108088+pawanbhure19@users.noreply.github.com>
2025-09-25 20:38:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@
 ##
 test2
 test3
+test4
 module "aws_reverse_proxy" {
   # Available inputs: https://github.com/futurice/terraform-utils/tree/master/aws_reverse_proxy#inputs
   # Check for updates: https://github.com/futurice/terraform-utils/compare/v11.0...master
